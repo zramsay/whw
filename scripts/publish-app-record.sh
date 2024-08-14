b398dac1d03967f4842f1ad70ef19bff9639c7cd
Thomas E Lackey <telackey@bozemanpass.com>
2024-08-14 16:31:12 -0500

    CI

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -19,15 +19,15 @@ rcd_app_version=$(jq -r '.version' package.json | sed 's/null//')
 
 cat <<EOF > "$CONFIG_FILE"
 services:
-  cns:
-    restEndpoint: '${CERC_REGISTRY_REST_ENDPOINT:-http://console.laconic.com:1317}'
-    gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-http://console.laconic.com:9473/api}'
-    chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic_9000-1}
-    gas: 2950000
-    fees: 400000aphoton
+  registry:
+    rpcEndpoint: '${CERC_REGISTRY_RPC_ENDPOINT:-http://testnet-a-1.dev.vaasl.io:26657}'
+    gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-http://testnet-a-1.dev.vaasl.io:9473/api}'
+    chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic-08062024}
+    gas: 900000
+    fees: 900000alnt
 EOF
 
-next_ver=$(laconic -c $CONFIG_FILE cns record list --type ApplicationRecord --all --name "$rcd_name" 2>/dev/null | jq -r -s ".[] | sort_by(.createTime) | reverse | [ .[] | select(.bondId == \"$CERC_REGISTRY_BOND_ID\") ] | .[0].attributes.version" | awk -F. -v OFS=. '{$NF += 1 ; print}')
+next_ver=$(laconic -c $CONFIG_FILE registry record list --type ApplicationRecord --all --name "$rcd_name" 2>/dev/null | jq -r -s ".[] | sort_by(.createTime) | reverse | [ .[] | select(.bondId == \"$CERC_REGISTRY_BOND_ID\") ] | .[0].attributes.version" | awk -F. -v OFS=. '{$NF += 1 ; print}')
 
 if [ -z "$next_ver" ] || [ "1" == "$next_ver" ]; then
   next_ver=0.0.1
@@ -51,19 +51,19 @@ EOF
 
 
 cat $RECORD_FILE
-RECORD_ID=$(laconic -c $CONFIG_FILE cns record publish --filename $RECORD_FILE --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} | jq -r '.id')
+RECORD_ID=$(laconic -c $CONFIG_FILE registry record publish --filename $RECORD_FILE --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} | jq -r '.id')
 echo $RECORD_ID
 
-if [ -z "$CERC_REGISTRY_APP_CRN" ]; then
+if [ -z "$CERC_REGISTRY_APP_LRN" ]; then
   authority=$(echo "$rcd_name" | cut -d'/' -f1 | sed 's/@//')
   app=$(echo "$rcd_name" | cut -d'/' -f2-)
-  CERC_REGISTRY_APP_CRN="crn://$authority/applications/$app"
+  CERC_REGISTRY_APP_LRN="lrn://$authority/applications/$app"
 fi
 
-laconic -c $CONFIG_FILE cns name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${rcd_app_version}" "$RECORD_ID"
-laconic -c $CONFIG_FILE cns name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${CERC_REPO_REF}" "$RECORD_ID"
+laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_LRN@${rcd_app_version}" "$RECORD_ID"
+laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_LRN@${CERC_REPO_REF}" "$RECORD_ID"
 if [ "true" == "$CERC_IS_LATEST_RELEASE" ]; then
-  laconic -c $CONFIG_FILE cns name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN" "$RECORD_ID"
+  laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_LRN" "$RECORD_ID"
 fi
 
 rm -f $RECORD_FILE $CONFIG_FILE
--- scripts/request-app-deployment.sh
+++ scripts/request-app-deployment.sh
@@ -10,23 +10,23 @@ rcd_app_version=$(jq -r '.version' package.json | sed 's/null//')
 
 cat <<EOF > "$CONFIG_FILE"
 services:
-  cns:
-    restEndpoint: '${CERC_REGISTRY_REST_ENDPOINT:-http://console.laconic.com:1317}'
-    gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-http://console.laconic.com:9473/api}'
-    chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic_9000-1}
-    gas: 2950000
-    fees: 400000aphoton
+  registry:
+    rpcEndpoint: '${CERC_REGISTRY_RPC_ENDPOINT:-http://testnet-a-1.dev.vaasl.io:26657}'
+    gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-http://testnet-a-1.dev.vaasl.io:9473/api}'
+    chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic-08062024}
+    gas: 900000
+    fees: 900000alnt
 EOF
 
-if [ -z "$CERC_REGISTRY_APP_CRN" ]; then
+if [ -z "$CERC_REGISTRY_APP_LRN" ]; then
   authority=$(echo "$rcd_name" | cut -d'/' -f1 | sed 's/@//')
   app=$(echo "$rcd_name" | cut -d'/' -f2-)
-  CERC_REGISTRY_APP_CRN="crn://$authority/applications/$app"
+  CERC_REGISTRY_APP_LRN="lrn://$authority/applications/$app"
 fi
 
-APP_RECORD=$(laconic -c $CONFIG_FILE cns name resolve "$CERC_REGISTRY_APP_CRN" | jq '.[0]')
+APP_RECORD=$(laconic -c $CONFIG_FILE cns name resolve "$CERC_REGISTRY_APP_LRN" | jq '.[0]')
 if [ -z "$APP_RECORD" ] || [ "null" == "$APP_RECORD" ]; then
-  echo "No record found for $CERC_REGISTRY_APP_CRN."
+  echo "No record found for $CERC_REGISTRY_APP_LRN."
   exit 1
 fi
 
@@ -35,9 +35,9 @@ record:
   type: ApplicationDeploymentRequest
   version: 1.0.0
   name: "$rcd_name@$rcd_app_version"
-  application: "$CERC_REGISTRY_APP_CRN@$rcd_app_version"
+  application: "$CERC_REGISTRY_APP_LRN@$rcd_app_version"
   dns: "$CERC_REGISTRY_DEPLOYMENT_SHORT_HOSTNAME"
-  deployment: "$CERC_REGISTRY_DEPLOYMENT_CRN"
+  deployment: "$CERC_REGISTRY_DEPLOYMENT_LRN"
   config:
     env:
       CERC_WEBAPP_DEBUG: "$rcd_app_version"
